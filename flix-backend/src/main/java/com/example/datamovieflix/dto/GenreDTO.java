f31d7e007ff09470fe9c979e7eabf43a1fb1aaf6
Admilson Ferreira <ferreiraad3@gmail.com>
2024-01-26 14:34:45 -0400

    GenreController, genreService

---
+++ flix-backend/src/main/java/com/example/datamovieflix/dto/GenreDTO.java
@@ -0,0 +1,43 @@
+package com.example.datamovieflix.dto;
+
+import com.example.datamovieflix.entities.Genre;
+
+public class GenreDTO {
+
+	private Long id;
+    private String name;
+    
+	public GenreDTO() {
+		super();
+	}
+
+	public GenreDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public GenreDTO(Genre genre) {
+		id = genre.getId();
+		name = genre.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+	
+    
+}
